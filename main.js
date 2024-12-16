a571433aff69af4b0eed3b398a68a07ffce544c1
Yasuhiro-Takeuchi <21j5081@stu.meisei-u.ac.jp>
2024-12-16 11:27:45 +0900

    test

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
 import {loadGLTF} from "./libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
+// document.addEventListener('DOMContentLoaded', () => {
+//   const start = async() => {
+//     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+//       container: document.body,
+//       imageTargetSrc: './assets/targets/open2.mind'
+//     });
+//     const {renderer, scene, camera} = mindarThree;
+
+//     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+//     scene.add(light);
+
+//     const house = await loadGLTF('./assets/models/house/usi.gltf');
+//     house.scene.scale.set(3, 3, 3);
+//     house.scene.position.set(0, 0, 0);
+//     house.scene.rotation.set(90, 0, 0);
+
+//     const houseAncor = mindarThree.addAnchor(0);
+//     houseAncor.group.add(house.scene);
+
+//     await mindarThree.start();
+//     renderer.setAnimationLoop(() => {
+//       renderer.render(scene, camera);
+//     });
+//   }
+//   //start();
+//   const startButton = document.createElement("button");
+//   startButton.textContent = "Start";
+//   startButton.addEventListener("click", start);
+//   document.body.appendChild(startButton);
+// });
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/o-pn.mind'
+      imageTargetSrc: './assets/targets/sample2.mind'
     });
     const {renderer, scene, camera} = mindarThree;
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const house = await loadGLTF('./assets/models/house/dog.gltf');
+    const house = await loadGLTF('./assets/models/house/uploads_files_3481608_dog_fur4.glb');
     house.scene.scale.set(3, 3, 3);
     house.scene.position.set(0, 0, 0);
     house.scene.rotation.set(90, 0, 0);
@@ -31,4 +64,3 @@ document.addEventListener('DOMContentLoaded', () => {
   startButton.addEventListener("click", start);
   document.body.appendChild(startButton);
 });
-
